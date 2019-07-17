d6f20eec6feec950eb225b277141a6f1d6ffc8b8
bulbasaur <shozemi.nishimoto@gmail.com>
2019-07-17 11:23:43 +0900

    Modify: small change

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -51,7 +51,7 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history : [{
-	squares: Array(9).fill(null),
+	  squares: Array(9).fill(null),
 	}],
 	stepNumber: 0,
 	xIsNext: true,
@@ -59,20 +59,20 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-  const history = this.state.history.slice(0, this.state.setpNumber + 1);
-  const current = history[history.length-1];
-  const squares = current.squares.slice();
-  if (calculateWinner(squares) || squares[i]) {
-    return;
-  }
-  squares[i] = this.state.xIsNext ? 'X' : 'O';
-  this.setState({
-	  history: history.concat([{
-		  squares: squares,
-	  }]),
-	  stepNumber: history.length,
-	  xIsNext: !this.state.xIsNext,
-  });
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
+      const current = history[history.length-1];
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
+      this.setState({
+          history: history.concat([{
+              squares: squares,
+          }]),
+          stepNumber: history.length,
+          xIsNext: !this.state.xIsNext,
+      });
   }
   
   jumpTo(step) {
@@ -109,9 +109,9 @@ class Game extends React.Component {
       <div className="game">
         <div className="game-board">
           <Board 
-		squares={current.squares}
+	    	squares={current.squares}
 	    	onClick={(i) => this.handleClick(i)}
-	  />
+	      />
 
         </div>
         <div className="game-info">
